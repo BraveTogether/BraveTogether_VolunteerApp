0af56b2c02ce1a55f83dc25f9e918f0bbf27c741
dvir lafer <dvirla84@gmail.com>
2020-09-13 15:23:59 +0300

    Trying to make facebook Login Work

--- app/src/main/java/com/example/bravetogether_volunteerapp/LoginActivity.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/LoginActivity.java
@@ -6,17 +6,55 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager = CallbackManager.Factory.create();
 
     TextView username = findViewById(R.id.usernameEditText);
     TextView email = findViewById(R.id.emailTextView);
     TextView password = findViewById(R.id.passwordEditText);
     TextView confirmPassword = findViewById(R.id.confirmPasswordEditText);
+    private LoginButton loginButton;
+//    private static final String EMAIL = "email";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+//        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
     }
 
     public void signIn(View view) {
