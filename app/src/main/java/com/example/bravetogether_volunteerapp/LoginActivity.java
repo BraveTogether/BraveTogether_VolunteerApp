f6e1b4321c93152edfc4dede71d0e725691e49f5
Zeronid <zeronid@gmail.com>
2020-09-14 13:21:04 +0300

    Login activity:   Moved the variable declarations inside the functions.

--- app/src/main/java/com/example/bravetogether_volunteerapp/LoginActivity.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/LoginActivity.java
@@ -9,23 +9,18 @@ import android.widget.TextView;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     CallbackManager callbackManager = CallbackManager.Factory.create();
 
-    TextView username = findViewById(R.id.usernameEditText);
-    TextView email = findViewById(R.id.emailTextView);
-    TextView password = findViewById(R.id.passwordEditText);
-    TextView confirmPassword = findViewById(R.id.confirmPasswordEditText);
-    private LoginButton loginButton;
-//    private static final String EMAIL = "email";
+    private TextView username;
+    private TextView email;
+    private TextView password;
+    private TextView confirmPassword;
+    private LoginButton facebookLoginButton;
 
 
     @Override
@@ -34,12 +29,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        loginButton = (LoginButton) findViewById(R.id.login_button);
+        facebookLoginButton = (LoginButton) findViewById(R.id.facebook_login_button);
 //        loginButton.setReadPermissions(Arrays.asList(EMAIL));
         // If you are using in a fragment, call loginButton.setFragment(this);
 
 //         Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
@@ -57,10 +52,21 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-//    public void signIn(View view) {
-//        String mUsername = username.getText().toString();
-//        String mEmail = email.getText().toString();
-//        String mPassword = password.getText().toString();
-//        String mConfirmPassword = confirmPassword.getText().toString();
-//    }
+    public void signIn(View view) { //Sign in using email and password
+
+        username = findViewById(R.id.usernameEditText);
+        email = findViewById(R.id.emailTextView);
+        password = findViewById(R.id.passwordEditText);
+        confirmPassword = findViewById(R.id.confirmPasswordEditText);
+
+
+        String mUsername = username.getText().toString();
+        String mEmail = email.getText().toString();
+        String mPassword = password.getText().toString();
+        String mConfirmPassword = confirmPassword.getText().toString();
+
+        //TODO
+        //Implement sending the data to the database,confirming it's valid,creating a user and signing him in.
+
+    }
 }
