a8caef6b5879025be6e25ceef4b40bfd16460a3f
noysherergib <noysherer2@gmail.com>
2020-09-17 12:35:15 +0300

    added missing files

--- app/src/main/java/com/example/bravetogether_volunteerapp/FilterActivity.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/FilterActivity.java
@@ -2,11 +2,21 @@ package com.example.bravetogether_volunteerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+import java.util.logging.Filter;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -29,7 +39,59 @@ public class FilterActivity extends AppCompatActivity {
         i.putExtra("duration", duration);
     }
 
+    public static LatLng getLocationFromAddress(Context context, String strAddress)
+    {
+        Geocoder coder= new Geocoder(context);
+        List<Address> address;
+        LatLng mylocation = null;
+        try
+        {
+            address = coder.getFromLocationName(strAddress, 5);
+            if(address==null)
+            {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            mylocation = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return mylocation;
+    }
+
+    public float getDistance(LatLng place1, LatLng place2) {
+        Location locationA = new Location("point A");
+
+        locationA.setLatitude(place1.latitude);
+        locationA.setLongitude(place1.longitude);
+
+        Location locationB = new Location("point B");
+
+        locationB.setLatitude(place1.latitude);
+        locationB.setLongitude(place1.longitude);
+
+        float distance = locationA.distanceTo(locationB);
+        return distance;
+    }
+
     public void Search(View view) {
+        //change addressUser for query to our db for the user address
+        //change addressFoundation for query to our db for the user address
+        String addressUser = "התקוה 23 רמת השרון";
+        String addressFoundation = "הרצל 22 הרצליה";
+
+        Context cont = getApplicationContext();
+        LatLng user = getLocationFromAddress(cont, addressUser);
+        LatLng foundation = getLocationFromAddress(cont, addressFoundation);
+
+        float distance = getDistance(foundation, user);
+        String test = String.valueOf(distance);
+
     }
 
     public void Save(View view) {
