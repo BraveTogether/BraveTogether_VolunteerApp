085c57a63017fa81b4ea2ef33318cbc024c5660d
Zeronid <zeronid@gmail.com>
2020-09-19 22:16:47 +0300

    Added a sample connection to the server in the Login activity  *SAMPLE CONNECTION*

--- app/src/main/java/com/example/bravetogether_volunteerapp/LoginActivity.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/LoginActivity.java
@@ -11,10 +11,15 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -23,6 +28,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.auth.api.Auth;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
@@ -66,8 +72,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         });
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
 
         googleSignInButton = findViewById(R.id.googleSignInButton);
         googleSignInButton.setOnClickListener(new View.OnClickListener() {
@@ -82,34 +88,52 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode,resultCode,data); //Facebook callback manager
-        if(requestCode == SIGN_IN){ //Google activity result
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if(result.isSuccess()){
-                startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                Toast.makeText(this, "You have logged in", Toast.LENGTH_SHORT).show();
-                finish();
-                //TODO implement the backend for the google login
-            }else{
-                Toast.makeText(this, "Login has failed", Toast.LENGTH_SHORT).show();
-            }
-
-        }
+//        callbackManager.onActivityResult(requestCode, resultCode, data); //Facebook callback manager
+//        if (requestCode == SIGN_IN) { //Google activity result
+//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+//            if (result.isSuccess()) {
+//                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+//                Toast.makeText(this, "You have logged in", Toast.LENGTH_SHORT).show();
+//                finish();
+//                //TODO implement the backend for the google login
+//            } else {
+//                Toast.makeText(this, "Login has failed", Toast.LENGTH_SHORT).show();
+//            }
+//
+//        }
     }
 
     public void signIn(View view) { //Sign in using email and password
-
-        String mUsername = username.getText().toString();
-        String mEmail = email.getText().toString();
-        String mPassword = password.getText().toString();
-        String mConfirmPassword = confirmPassword.getText().toString();
-
         //TODO Implement sending the data to the database,confirming it's valid,creating a user and signing him in.
         //Check if username not exist && email not exists && passwords match
+
+
+
+        //Getting Data From Localhost NodeJS Express Server --- Success!
+        RequestQueue queue = Volley.newRequestQueue(this); //A queue for our requests
+        String url = "http://localhost:3000/users"; //The url we are sending the get requests to
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() { //A single get request
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject obj = new JSONObject(response.substring(1,response.length()-1));
+                    Toast.makeText(LoginActivity.this, obj.get("vid").toString(),Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(stringRequest); //Adding our request to the Volley Queue so it can run
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) { //A google API method
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
