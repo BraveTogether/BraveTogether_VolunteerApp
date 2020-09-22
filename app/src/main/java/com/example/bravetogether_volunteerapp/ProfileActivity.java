1f9b60321ca827c7c7c948357122f2a536c2b464
Noy Sherer Gibstein <noysherer2@gmail.com>
2020-09-22 11:24:41 +0300

    added settings panel

--- app/src/main/java/com/example/bravetogether_volunteerapp/ProfileActivity.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/ProfileActivity.java
@@ -31,7 +31,7 @@ public class ProfileActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_about, R.id.nav_slideshow, R.id.nav_contact)
+                R.id.nav_home, R.id.nav_about, R.id.nav_settings, R.id.nav_contact)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
