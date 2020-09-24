bcd4ad96d28239b19b651ec94c73dbc972045b85
Zeronid <zeronid@gmail.com>
2020-09-24 12:32:06 +0300

    added drawables

---
+++ app/src/main/java/com/example/bravetogether_volunteerapp/CreateAccountFirstActivity.java
@@ -0,0 +1,14 @@
+package com.example.bravetogether_volunteerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountFirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_first);
+    }
+}
