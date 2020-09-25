8a890fc2d88b404e1da82bbd24f6771bcb7ba3a5
dvir lafer <dvirla84@gmail.com>
2020-09-25 17:35:00 +0300

    Started NotificationActivity

---
+++ app/src/main/java/LoginFlow/NotificationActivity.java
@@ -0,0 +1,20 @@
+package LoginFlow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bravetogether_volunteerapp.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+
+    public void expandTimeBox(View view) {
+    }
+}
