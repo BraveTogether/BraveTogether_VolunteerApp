0441284b7fae9a3daca08bca9cbf859799144153
dvir lafer <dvirla84@gmail.com>
2020-09-26 13:45:13 +0300

    NotificationActivity In Progress

--- app/src/main/java/LoginFlow/NotificationActivity.java
+++ app/src/main/java/LoginFlow/NotificationActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.example.bravetogether_volunteerapp.R;
 
@@ -13,6 +15,19 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+
+        Switch sw = (Switch) findViewById(R.id.switch_button);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                } else {
+                    // The toggle is disabled
+                }
+            }
+        });
+
     }
 
     public void expandTimeBox(View view) {
