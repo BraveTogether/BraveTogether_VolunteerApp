3a36c89a87e0a125d57a46454c82938519829d7f
dvir lafer <dvirla84@gmail.com>
2020-09-29 15:54:52 +0300

    NotificationActivity working on time box

--- app/src/main/java/com/example/bravetogether_volunteerapp/LoginFlow/NotificationActivity.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/LoginFlow/NotificationActivity.java
@@ -3,7 +3,6 @@ package com.example.bravetogether_volunteerapp.LoginFlow;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
