7a851d045983ab95bbd3f2107f6c12da7aac4d7f
Zeronid <zeronid@gmail.com>
2020-09-30 12:30:38 +0300

    1

--- app/src/main/java/com/example/bravetogether_volunteerapp/app/app.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/app/app.java
@@ -7,11 +7,9 @@ import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
 
 import com.example.bravetogether_volunteerapp.LoginFlow.IntroFirstTimeActivity;
-import com.example.bravetogether_volunteerapp.LoginFlow.LoginActivity;
 import com.example.bravetogether_volunteerapp.LoginFlow.RegisterActivity;
 import com.example.bravetogether_volunteerapp.MainActivity;
 import com.facebook.AccessToken;
-import com.facebook.login.Login;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
@@ -30,7 +28,10 @@ public class app extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //Context
+        checkForCurrentUserState();
+    }
+
+    public void checkForCurrentUserState(){
         acct = GoogleSignIn.getLastSignedInAccount(this);
         accessToken = AccessToken.getCurrentAccessToken();
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
@@ -42,11 +43,13 @@ public class app extends Application {
             if (acct != null) {
                 Intent intent = new Intent(app.this, MainActivity.class);
                 uid = acct.getId();
+                //Implement call to server to check if user signed up or not
                 intent.putExtra("uid", uid);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             } else if (accessToken != null) {
                 uid = accessToken.getUserId();
+                //Implement call to server to check if user signed up or not
                 Intent intent = new Intent(app.this, MainActivity.class);
                 intent.putExtra("uid", uid);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -54,6 +57,7 @@ public class app extends Application {
             } else if (prefs.getString("uid", null) != null) {
                 Intent intent = new Intent(app.this, MainActivity.class);
                 uid = prefs.getString("uid", null);
+                //Implement call to server to get all the user data
                 intent.putExtra("uid", uid);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
