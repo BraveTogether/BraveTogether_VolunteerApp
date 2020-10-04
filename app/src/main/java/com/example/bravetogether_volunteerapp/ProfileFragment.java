96495d66a37cdd5a404b839869dfe9ea851b61fe
Noy Sherer Gibstein <noysherer2@gmail.com>
2020-10-04 17:57:46 +0300

    added regular user fragment

--- app/src/main/java/com/example/bravetogether_volunteerapp/ProfileFragment.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/ProfileFragment.java
@@ -2,12 +2,14 @@ package com.example.bravetogether_volunteerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
 import com.example.bravetogether_volunteerapp.LoginFlow.RegisterActivity;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
