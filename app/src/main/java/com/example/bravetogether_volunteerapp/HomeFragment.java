8ee5d169e76d0d6a14d6a7238d8690081aaa5b7a
dvir lafer <dvirla84@gmail.com>
2020-10-04 15:43:44 +0300

    Home is working

--- app/src/main/java/com/example/bravetogether_volunteerapp/HomeFragment.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/HomeFragment.java
@@ -4,10 +4,6 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
@@ -55,10 +51,10 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_home, container, false);
+//    }
 }
