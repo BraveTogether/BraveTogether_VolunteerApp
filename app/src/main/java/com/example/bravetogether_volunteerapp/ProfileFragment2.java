af59311129fbc5e11babc62a63dd12eed06f74b2
Tamirshazman <A0547799597t>
2020-10-13 16:39:53 +0300

    Merge branch 'ben' of C:\Users\User\Desktop\HomeWork\BraveTogether_VolunteerApp with conflicts.

--- app/src/main/java/com/example/bravetogether_volunteerapp/ProfileFragment2.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/ProfileFragment2.java
@@ -1,101 +1,46 @@
 package com.example.bravetogether_volunteerapp;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.bravetogether_volunteerapp.adapters.ProfileFragmentEventAdapter;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.bravetogether_volunteerapp.adapters.profileTagAdapter;
 
 import java.util.ArrayList;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ProfileFragment2 extends Fragment {
+public class ProfileFragment2 extends Fragment  {
 
-    // Variables:
-   private CircleImageView mProfileImage;
-   private TextView mUserName, mVolunteerHours, mCoins, mProcess;
-   private  View ProfileView;
-   private RecyclerView rcTags,rcNearVolunteers,rcVolunteerHistory;
-   private Context context;
-    public ProfileFragment2(){}
+    private ArrayList<String> mTagName = new ArrayList<>();
+    private ArrayList<String> mTagDetails = new ArrayList<>();
+    private ArrayList<Integer> mTagImage = new ArrayList<>();
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        ProfileView = inflater.inflate(R.layout.fragment_profile2, container, false);
-        setUserDate();
-        setNearVoluteer();
-        setVolunteerHistory();
-        return ProfileView;
-    }
-
-
-
-    // Access to DB to get user data
-    private void setUserDate(){
-        // init variables
-        mProfileImage = (CircleImageView) ProfileView.findViewById(R.id.imageViewProfilePic);
-        mUserName = (TextView) ProfileView.findViewById(R.id.textViewUserName);
-        mVolunteerHours = (TextView) ProfileView.findViewById(R.id.txtHours);
-        mCoins = (TextView) ProfileView.findViewById(R.id.txtCoins);
-        mProcess = (TextView) ProfileView.findViewById(R.id.txtProcess);
-        // TODO:: poll user data from server and set variables
-    }
-
-    private void setVolunteerHistory(){
-        // TODO:: poll volunteer history from server.
-        ArrayList<VolunteerEvent> list = createDummyList(); // need to change to correct list.
-        rcVolunteerHistory = ProfileView.findViewById(R.id.rcVolunteerHistory);
-        ProfileFragmentEventAdapter adapter = new ProfileFragmentEventAdapter(context,list);
-        setRecyclerViewSetting(rcVolunteerHistory,adapter);
-
-    }
-
-    private void setNearVoluteer(){
-        // TODO:: check user location and define near location (what is the radius)
-        // TODO:: poll near volunteer from server.
-        ArrayList<VolunteerEvent> list = createDummyList(); // need to change to correct list.
-        rcNearVolunteers = ProfileView.findViewById(R.id.rcNearVolunteer);
-        ProfileFragmentEventAdapter adapter = new ProfileFragmentEventAdapter(context,list);
-        setRecyclerViewSetting(rcNearVolunteers,adapter);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        this.context = context;
-    }
-
-    // Dummy function for test ** need to be deleted **
-    private ArrayList<VolunteerEvent> createDummyList(){
-        ArrayList<VolunteerEvent> dummyList =  new ArrayList<>();
-        dummyList.add(new VolunteerEvent("עזרה בקניות","13/14/15","גיל הזהב", "120" ,"3" , "300"));
-        dummyList.add(new VolunteerEvent("עזרה בקניות","13/14/15","גיל הזהב", "120" ,"3" , "300"));
-        dummyList.add(new VolunteerEvent("עזרה בקניות","13/14/15","גיל הזהב", "120" ,"3" , "300"));
-        return dummyList;
-    }
-
-    private void setRecyclerViewSetting(RecyclerView view, RecyclerView.Adapter adapter){
-        view.setHasFixedSize(true);
-        view.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false ));
-        view.setAdapter(adapter);
-        view.setNestedScrollingEnabled(false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Set listeners for the cilckables
+        View v = inflater.inflate(R.layout.fragment_profile2, container, false);
+        mTagName.add("ארת גבורה");
+        mTagDetails.add("30 שעות התנדבות");
+        mTagImage.add(getResources().getIdentifier("@drawable/component400",null,"com.example.bravetogether_volunteerapp" ));
+        RecyclerView recyclerView = v.findViewById(R.id.my_recycler_view);
+        profileTagAdapter profileTagAdapter = new profileTagAdapter(this.getContext(), mTagName, mTagDetails, mTagImage);
+        recyclerView.setAdapter(profileTagAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        return v;
     }
 
 }
