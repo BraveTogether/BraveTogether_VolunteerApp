e875a5d566fc009a1b6d288aec5864cd36fd64ae
shriki <benshriki93@gmail.com>
2020-10-14 19:06:15 +0300

    ProfileFragment2 fix

--- app/src/main/java/com/example/bravetogether_volunteerapp/ProfileFragment2.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/ProfileFragment2.java
@@ -1,12 +1,16 @@
 package com.example.bravetogether_volunteerapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
@@ -15,10 +19,24 @@ import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.bravetogether_volunteerapp.adapters.ProfileFragmentEventAdapter;
 import com.example.bravetogether_volunteerapp.adapters.Tags_ProfileFragment_Adapter;
+import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,6 +48,8 @@ public class ProfileFragment2 extends Fragment {
    private  View ProfileView;
    private RecyclerView rcTags,rcNearVolunteers, rcMyVolunteer;
    private Context context;
+   private List<JSONObject> dbNearEvents;
+   private final int radius = 10; // in KM
 
    // DB and sharedPrefFile:
    //private final String url = getResources().getString(R.string.apiUrl);
@@ -45,7 +65,6 @@ public class ProfileFragment2 extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         ProfileView = inflater.inflate(R.layout.fragment_profile2, container, false);
-        mPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         setUserDate();
         setNearVolunteer();
         setMyVolunteers();
@@ -58,6 +77,9 @@ public class ProfileFragment2 extends Fragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         this.context = context;
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        setNearEvents();
+
     }
 
     // Access to DB / SharedFile to get user data
@@ -103,7 +125,7 @@ public class ProfileFragment2 extends Fragment {
     private void setNearVolunteer(){
         // TODO:: check user location and define near location (what is the radius)
         // TODO:: poll near volunteer from server.
-        ArrayList<ProfileEventObject> list = createDummyEventList(); // need to change to correct list.
+        ArrayList<ProfileEventObject> list = getNearVolunteerlist(); // need to change to correct list.
         rcNearVolunteers = ProfileView.findViewById(R.id.rcNearVolunteer);
         ProfileFragmentEventAdapter adapter = new ProfileFragmentEventAdapter(context,list);
         setRecyclerViewSetting(rcNearVolunteers,adapter);
@@ -123,6 +145,81 @@ public class ProfileFragment2 extends Fragment {
         view.setNestedScrollingEnabled(false);
     }
 
+    private ArrayList<ProfileEventObject> getNearVolunteerlist(){
+        ArrayList<ProfileEventObject> list = new ArrayList<>();
+        for (JSONObject event : dbNearEvents){
+            list.add(new ProfileEventObject(event));
+        }
+        return list;
+    }
+
+    // copied from FillterActivity if function become static we can delete this.
+    public float getDistance(String address1, String address2) {
+
+        LatLng place1 = FilterActivity.getLocationFromAddress(context, address1);
+        LatLng place2 = FilterActivity.getLocationFromAddress(context, address2);
+        Location locationA = new Location("location");
+
+        locationA.setLatitude(place1.latitude);
+        locationA.setLongitude(place1.longitude);
+
+        Location locationB = new Location("location");
+
+        locationB.setLatitude(place2.latitude);
+        locationB.setLongitude(place2.longitude);
+
+
+        return locationA.distanceTo(locationB);
+    }
+    // modified Search method from FillterActivity -  set dbNearEvent list with all the near event.
+    private void setNearEvents(){
+        final String userAddress = mPreferences.getString("userAddress",
+                "Remez 8, Tel Aviv, Israel");  // default address may need to change.
+        try{
+            String URL = getResources().getString(R.string.apiUrl) + "volunteers/confirmed/1";
+            JsonArrayRequest JsonArrayRequest = new JsonArrayRequest
+                    (Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
+                        JSONObject Activity;
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            dbNearEvents = new ArrayList<JSONObject>();
+                            boolean distance;
+                                for (int i = 0; i < response.length(); i++) {
+                                    try {
+                                        Activity = response.getJSONObject(i);
+                                        int online=Activity.getInt("online"); // 1 equals true
+                                        float dist=0;
+                                        if (online==0 )
+                                        {
+                                            //calculate the distance between the user and the volunteer.
+                                            dist = getDistance(Activity.get("address").toString(), userAddress);
+                                        }
+                                        distance = dist < (radius*1000);
+                                        if (distance ) {
+                                            // add the distance from user to the volunteer data being forwarded to the next activity
+                                            Activity.put("distance_from_user", String.valueOf(dist));
+                                            dbNearEvents.add((JSONObject)Activity);
+                                            //Log.e("Activity", activities.toString());
+                                        }
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            Intent intent = new Intent(context, ProfileFragment2.class);
+                            startActivity(intent);
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("message", "Failed with error msg:\t" + error.getMessage());
+                            Log.e("stacktrace", "Error StackTrace: \t" + error.getStackTrace());
+                            Log.e("Response", "there was an error in setNearEvents :(");
+                        }
+                    });
+        }
+        catch (Exception e){}
+    }
 
     public  class ProfileEventObject{
         private String headline;
@@ -141,6 +238,21 @@ public class ProfileFragment2 extends Fragment {
             this.imgUrl = imgUrl;
             this.uid = uid;
             this.credits=credits;
+        }
+
+        public ProfileEventObject(JSONObject activity){
+            try {
+                this.headline = activity.getString("name");
+                this.start_time = activity.get("start_time").toString(); // TODO:: check format.
+                this.location = activity.getString("address");
+                this.imgUrl = activity.getString("picture");
+                this.uid = activity.getLong("id");
+                this.credits = activity.getInt("value_in_coins")+"";
+                this.date = "dummydate"; // TODO:: get correct date.
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
 
         public String getCredits() {
