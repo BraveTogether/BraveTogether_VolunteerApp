3faafc49b18756ae35fd5223d57e552b86d0584a
Tamirshazman <A0547799597t>
2020-10-29 18:51:56 +0200

    change queries

--- app/src/main/java/com/example/bravetogether_volunteerapp/EditNotificationDetails.java
+++ app/src/main/java/com/example/bravetogether_volunteerapp/EditNotificationDetails.java
@@ -29,18 +29,20 @@ import java.util.ArrayList;
 public class EditNotificationDetails extends AppCompatActivity {
 
     // variable that holds the details of the user volunteer,
-    //in the onCreate function the variable updated from the shared preferences file
-    private Integer timesOfVolunteer;//1-morning, 2-noon, 3-evening, 4-no_limit
-    private Integer typeOfVolunteer;//1-online, 2-close, 3 anywhere
+    private Integer distanceRadius = 500;//radius preference
+    private Integer timesOfVolunteer = 3;//1-morning, 2-noon, 3-evening, 4-no_limit
+    private Integer typeOfVolunteer = 1;//1-online, 2-close, 3 anywhere
     private BigInteger uid;// id
     private BigInteger id;
     //url for server requests
     final String url = "http://35.214.78.251:8080/";
 
     //ArrayList the holds all the button for easy iteration.
+    ArrayList<TextView> buttonsDistanceRadius = new ArrayList<>();
     ArrayList<TextView> buttonsTimesOfVolunteer = new ArrayList<>();
     ArrayList<TextView> buttonsTypeOfVolunteer = new ArrayList<>();
 
+    static boolean c = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -51,12 +53,15 @@ public class EditNotificationDetails extends AppCompatActivity {
         String sharedPrefFile = "com.example.android.BraveTogether_VolunteerApp";
         SharedPreferences mPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE);
         //TODO: check if all the mPreferences names are correct
-        timesOfVolunteer = mPreferences.getInt("user_notification_dates", 1);
-        typeOfVolunteer = mPreferences.getInt("type", 1);
-        uid = new BigInteger(String.valueOf(mPreferences.getInt("id", 8)));
+        uid = new BigInteger(String.valueOf(mPreferences.getInt("id", 64)));
         //TODO: check if all the mPreferences names are correct
 
         //Add all buttons
+        buttonsDistanceRadius.add((TextView)this.findViewById(R.id.distance10));
+        buttonsDistanceRadius.add((TextView)this.findViewById(R.id.distance15));
+        buttonsDistanceRadius.add((TextView)this.findViewById(R.id.distance20));
+        buttonsDistanceRadius.add((TextView)this.findViewById(R.id.distanceUnlimited));
+
         buttonsTimesOfVolunteer.add((TextView)this.findViewById(R.id.timesMorning));
         buttonsTimesOfVolunteer.add((TextView)this.findViewById(R.id.timesNoon));
         buttonsTimesOfVolunteer.add((TextView)this.findViewById(R.id.timesEvening));
@@ -66,17 +71,26 @@ public class EditNotificationDetails extends AppCompatActivity {
         buttonsTypeOfVolunteer.add((TextView)this.findViewById(R.id.typeClose));
         buttonsTypeOfVolunteer.add((TextView)this.findViewById(R.id.typeUnlimited));
 
-        setTimesOfVolunteer(null);
-        setTypeOfVolunteer(null);
 
-        //request the profile ID form DB
+
+
+
         //TODO check how to use specific query
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url + "users_notifications/get_ids/"+ uid , null, new Response.Listener<JSONObject>() {
+                (Request.Method.GET, url + "users_notifications/get_ids_and_preference/"+ uid , null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("Gotttttt","id"+id.toString());
-                        id = new BigInteger(response.toString());
+                        Log.d("success_response", response.toString());
+                        try {
+                            id = BigInteger.valueOf(response.getInt("id"));
+                            typeOfVolunteer = response.getInt("notification_location_pref_id");
+                            distanceRadius = response.getInt("distance");
+                            c = false;
+                        }
+
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },new  Response.ErrorListener(){
                     @Override
@@ -86,48 +100,77 @@ public class EditNotificationDetails extends AppCompatActivity {
                     }
         });
         VolleySingleton.getInstance(EditNotificationDetails.this).addToRequestQueue(jsonObjectRequest);
+
+        //request the profile ID form DB don't exit while loop if response from server not got back
+        while (c){};
+
+        JsonObjectRequest jsonObjectRequest2 = new JsonObjectRequest
+                (Request.Method.GET, url + "users_notifications/get_times/"+ id , null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("success_response", response.toString());
+                        try {
+                         timesOfVolunteer = response.getInt("users_notifications_id");
+                        }
+
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },new  Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+        VolleySingleton.getInstance(EditNotificationDetails.this).addToRequestQueue(jsonObjectRequest2);
+
+        setDistanceRadius(null);
+        setTimesOfVolunteer(null);
+        setTypeOfVolunteer(null);
     }
 
 
 
-    //this function might be use in the future
+
     /**
      * Called when a view(distanceRadius) has been clicked.
      *Change the color of the button and set variable.
      * @param view The view that was clicked, if view is null, function called in onCreate to set
      *         the view of the page
      */
-//    public void setDistanceRadius(View view){
-//        int position = 0;
-//        String [] s = {"10", "15", "20", "unlimited"};
-//        if (view != null) {
-//            for (View v : buttonsDistanceRadius) {
-//                if (v.getId() == view.getId()) {
-//                    v.setBackgroundResource(R.drawable.buttonblueinside);
-//                    distanceRadius = s[position];
-//                }
-//                else
-//                    v.setBackgroundResource(R.drawable.textfield_d_buttons);
-//                position++;
-//            }
-//        }
-//        else {
-//            switch (distanceRadius) {
-//                case "10":
-//                    buttonsDistanceRadius.get(0).setBackgroundResource(R.drawable.buttonblueinside);
-//                    break;
-//                case "15":
-//                    buttonsDistanceRadius.get(1).setBackgroundResource(R.drawable.buttonblueinside);
-//                    break;
-//                case "20":
-//                    buttonsDistanceRadius.get(2).setBackgroundResource(R.drawable.buttonblueinside);
-//                    break;
-//                case "Unlimited":
-//                    buttonsDistanceRadius.get(3).setBackgroundResource(R.drawable.buttonblueinside);
-//                    break;
-//            }
-//        }
-//    }
+    public void setDistanceRadius(View view){
+        int position = 0;
+        Integer [] radius = {10, 15, 20, 500};
+        if (view != null) {
+            for (View v : buttonsDistanceRadius) {
+                if (v.getId() == view.getId()) {
+                    v.setBackgroundResource(R.drawable.buttonblueinside);
+                    distanceRadius = radius[position];
+                }
+                else
+                    v.setBackgroundResource(R.drawable.textfield_d_buttons);
+                position++;
+            }
+        }
+        else {
+            switch (distanceRadius) {
+                case 10:
+                    buttonsDistanceRadius.get(0).setBackgroundResource(R.drawable.buttonblueinside);
+                    break;
+                case 15:
+                    buttonsDistanceRadius.get(1).setBackgroundResource(R.drawable.buttonblueinside);
+                    break;
+                case 20:
+                    buttonsDistanceRadius.get(2).setBackgroundResource(R.drawable.buttonblueinside);
+                    break;
+                case 500:
+                    buttonsDistanceRadius.get(3).setBackgroundResource(R.drawable.buttonblueinside);
+                    break;
+            }
+        }
+    }
 
     /**
      * Called when a view(setTimesVolunteer) has been clicked.
@@ -198,9 +241,11 @@ public class EditNotificationDetails extends AppCompatActivity {
 
     //TODO need to edit the profile on the DB
     public void submitEdit(View view){
+        JSONObject distancePref = new JSONObject();
         JSONObject locationPref = new JSONObject();
         JSONObject timePref = new JSONObject();
         try {
+            distancePref.put("distance", distanceRadius);
             locationPref.put("location_pref_id", typeOfVolunteer);
             timePref.put("time_window", timesOfVolunteer);
 
